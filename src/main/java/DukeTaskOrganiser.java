b1c0f025ef74e6b1509f954edb47574dcd07ccbd
wizjoon <36699135+wizjoon@users.noreply.github.com>
2022-04-11 01:07:23 +0800

    Add files via upload

--- src/main/java/DukeTaskOrganiser.java
+++ src/main/java/DukeTaskOrganiser.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 public class DukeTaskOrganiser {
     private static ArrayList <Task> toDoTaskList = new ArrayList();
+    private static int taskCounter = 0;
     //private String [] todoList = new String[100];
 
     public DukeTaskOrganiser(){
     }
 
-    public void dukeMenu(){
+    public void dukeMenu() {
 
         Scanner readUserInput = new Scanner(System.in);
         String readUserRespond = "";
 
         boolean systemStatus = true;
-        int taskCounter = 0;
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -24,33 +24,55 @@ public class DukeTaskOrganiser {
         System.out.println("Hello from\n" + logo);
 
         System.out.println("Hello! I'm Duke");
-        while (systemStatus){
-            System.out.println("What can I do for you?");
-            readUserRespond =  readUserInput.nextLine();
+        System.out.println("What can I do for you?");
 
-            if(readUserRespond.toLowerCase().equals("bye")){
+        while (systemStatus) {
+            readUserRespond = readUserInput.nextLine();
+
+            if (readUserRespond.toLowerCase().equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 systemStatus = false;
                 break;
             }
-            if(readUserRespond.isBlank()){
-                System.out.println("No Resonse from you!!");
-            } else if(readUserRespond.toLowerCase().equals("list")){
-                if(taskCounter == 0) {
-                    System.out.println("Your tasklist is currently empty");
+            if (readUserRespond.isBlank()) {
+                System.out.println("No Response Received!!");
+                continue;
+            } else if (readUserRespond.toLowerCase().equals("list")) {
+                printTaskList();
+            } else if (readUserRespond.toLowerCase().startsWith("mark") || readUserRespond.toLowerCase().startsWith("unmark")) {
+                try {
+                    String[] words = readUserRespond.split(" ");
+                    if (words.length == 2) {
+                        int choiceTask = Integer.parseInt(words[1]);
+                        if (words[0].equals("mark")) {
+                            toDoTaskList.get(choiceTask - 1).setDone();
+                        } else if (words[0].equals("unmark")) {
+                            toDoTaskList.get(choiceTask - 1).setUndone();
+                        }
+                    }
+                } catch (NumberFormatException ex) {
+                    System.out.println("Please enter a number for your choice");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("Index out of bound");
                 }
-                System.out.println("Here are the tasks in your list:");
-                for(int i=0; i<taskCounter; i++){
-                    System.out.println(i+1 + ". " + toDoTaskList.get(i).getTaskStatus());
-                }
-
-            } else{
+            } else {
                 //todoList[taskCounter] = readUserRespond;
                 Task newTask = new Task(readUserRespond);
                 toDoTaskList.add(newTask);
                 System.out.println("added: " + readUserRespond);
                 taskCounter += 1;
             }
+        }
+    }
+
+    public void printTaskList() {
+        if (taskCounter == 0) {
+            System.out.println("Your tasklist is currently empty");
+            return;
+        }
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.println(i + 1 + ". " + toDoTaskList.get(i).getTaskStatus());
         }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,7 +15,7 @@ public class Task {
 
     public void setUndone(){
         this.isDone = false;
-        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("OK, I've marked this task as not done yet:");
         System.out.println("  [ ]" + " " + this.description);
     }
 
